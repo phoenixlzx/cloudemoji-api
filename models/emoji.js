60f0b505069d23ad27eecd7e4621923598c477ff
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-06-17 11:43:18 +0800

    added emoji model

---
+++ models/emoji.js
@@ -0,0 +1,69 @@
+/**
+ * 2014-06-16 20:33 UTC+8
+ * Phoenix Nemo <i@phoenixlzx.com>
+ * License MIT | http://opensource.org/licenses/MIT
+ */
+
+/*
+* emoji document
+* {
+*   _id: ObjectID(),
+*   emoji: 'ლ(╹◡╹ლ)',
+*   desc: {
+*       'ja': '説明',
+*       'en': 'description',
+*       'zh': '描述'
+*   },
+*   shortcuts: [
+*       'typingkey1',
+*       'typingkey2'
+*   ]
+* }
+* */
+
+var config = require('../config');
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(config.mongodb, { db: { native_parser: true, w : 1 } }, function(err, db) {
+
+    if (err) {
+        throw err;
+    }
+
+    var collection = db.collection('emoji');
+
+    exports.update = function(emoji, callback) {
+
+        collection.update({
+            emoji: emoji.emoji
+        }, {
+            emoji: emoji.emoji,
+            desc: emoji.desc,
+            keys: emoji.shortcuts
+        }, {
+            upsert: true
+        }, function(err) {
+
+            if (err) {
+                console.log(err);
+                return callback(err);
+            }
+
+            collection.ensureIndex({
+                emoji: 1,
+                keys: 1
+            }, function(err) {
+
+                if (err) {
+                    console.log(err);
+                    return callback(err);
+                }
+
+                callback();
+
+            });
+
+        });
+    }
+
+});
